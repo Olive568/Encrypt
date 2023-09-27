ebd460c4bb62f5ee1e40281ef308149080216a95
Olive568 <luislabapis@gmail.com>
2023-09-27 17:59:56 +0800

    Update 0.5

--- Encrypt/Program.cs
+++ Encrypt/Program.cs
@@ -3,30 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 namespace Encrypt
 {
     internal class Program
     {
-        static char[] ALPH = new char[] {'A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        static char[] ALPH = new char[] {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         static List<char> ALPHABET = new List<char>();
         static void Main(string[] args)
-        {
-            Console.WriteLine("Type E to Encrypt and D to Decrypt");
-            string choice = Console.ReadLine();
-            choice = choice.ToUpper();
-            switch (choice) 
-            {
-                case "E":
-                    string Key = GetKey();
-                    ALPHABET = EncryptedALPH(ALPH,Key);
-                    Console.WriteLine("write the word you want to encrypt");
-                    string Word = Console.ReadLine();
-                    Word.ToUpper();
-                    string final = encrypter(Word, ALPH, ALPHABET);
-                    Console.WriteLine(final);
-                    Console.ReadKey();
-                    break;
-            }
+        {     
+            Choice();
         }
         static string GetKey()
         {
@@ -36,8 +22,7 @@ namespace Encrypt
             {
                 loop = false;
                 Console.WriteLine("Write Your Key");
-                Key = Console.ReadLine();
-                Key.ToUpper();
+                Key = Console.ReadLine().ToUpper();
                 for (int i = 0; i < Key.Length; i++)
                 {
                     char c = Key[i];
@@ -51,7 +36,6 @@ namespace Encrypt
                 }
             }
             return Key;
-
         }
         static List<char> EncryptedALPH(char[] ALPH, String Key)
         {
@@ -81,7 +65,7 @@ namespace Encrypt
             {
                 char c = Word[i];
                 int cint = (int)c;
-                if(cint < 65 || cint > 90)
+                if(cint > 65 && cint < 90)
                 {
                     for (int x = 0; x < ALPH.Length; x++)
                     {
@@ -89,7 +73,6 @@ namespace Encrypt
                         {
                             encword += ALPHABET[x];
                             break;
-
                         }
                     }
                 }
@@ -99,6 +82,101 @@ namespace Encrypt
                 }
             }
             return encword;
+        }
+        static string Decrypter(String Word, char[] ALPH, List<char> ALPHABET)
+        {
+            string encword = "";
+            Word.ToUpper();
+            for(int i = 0; i < Word.Length; i++)
+            {
+                char c = Word[i];
+                int cint = (int)c;
+                if(cint > 65 && cint < 90)
+                {
+                    for(int x = 0; x < ALPH.Length; x++)
+                    {
+                        if(c == ALPHABET[x])
+                        {
+                            encword += ALPH[x];
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    encword += c;
+                }
+            }
+            return encword;
+        }
+        static void Choice()
+        {
+            Console.Clear();
+            Console.WriteLine("Type E to Encrypt and D to Decrypt");
+            string choice = Console.ReadLine().ToUpper();
+            switch (choice)
+            {
+                case "E":
+                    string Key = GetKey();
+                    ALPHABET = EncryptedALPH(ALPH, Key);
+                    Console.WriteLine("write the word you want to encrypt");
+                    string Word = Console.ReadLine().ToUpper() ;
+                    string final = encrypter(Word, ALPH, ALPHABET);
+                    Console.WriteLine(final);
+                    Write(final);
+                    Console.ReadKey();
+                    break;
+                case "D":
+                    Key = GetKey();
+                    ALPHABET = EncryptedALPH(ALPH, Key);
+                    Word = Read();
+                    final = Decrypter(Word, ALPH, ALPHABET);
+                    
+                    foreach (char c in ALPH)
+                        Console.Write(c + " ");
+                    Console.WriteLine();
+                    foreach (char c in ALPHABET)
+                        Console.Write(c + " ");
+
+                        Console.WriteLine(final);
+                    Console.ReadKey();
+                    break;
+                default:
+
+                    break;
+            }
+            Console.Clear();
+            Console.WriteLine("do you want to continue? Y/N");
+            string a = Console.ReadLine().ToUpper();
+            switch (a)
+                {
+                case "Y":
+                    Choice();
+                    break;
+                case "N":
+
+                    break;
+            }
+        }
+        static void Write(String final)
+        {
+            using (StreamWriter sw = new StreamWriter("eMessage.txt"))
+            {
+                sw.Write(final);
+            }
+        }
+        static string Read()
+        {
+            string total = "";
+            string line = "";
+            using (StreamReader sr = new StreamReader("eMessage.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    total += line;
+                }
+            }
+            return total;
         }
     }
 }
